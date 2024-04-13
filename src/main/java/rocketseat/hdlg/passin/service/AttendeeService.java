0ec37cb400fc419e50ac94d10d9e6a522b8a30f5
hidelgionovela <hidelgionovela@gmail.com>
2024-04-13 21:07:27 +0200

    chore: update AttendeeService

--- src/main/java/rocketseat/hdlg/passin/service/AttendeeService.java
+++ src/main/java/rocketseat/hdlg/passin/service/AttendeeService.java
@@ -2,12 +2,15 @@ package rocketseat.hdlg.passin.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 import rocketseat.hdlg.passin.domain.attendee.Attendee;
+import rocketseat.hdlg.passin.domain.attendee.exceptions.AttendeeAlreadyExistException;
+import rocketseat.hdlg.passin.domain.attendee.exceptions.AttendeeNotFoundException;
 import rocketseat.hdlg.passin.domain.checkIn.CheckIn;
+import rocketseat.hdlg.passin.dto.attendee.AttendeeBadgeDTO;
 import rocketseat.hdlg.passin.dto.attendee.AttendeeDetails;
 import rocketseat.hdlg.passin.dto.attendee.AttendeesListResponseDTO;
 import rocketseat.hdlg.passin.repositories.AttendeeRepository;
-import rocketseat.hdlg.passin.repositories.CheckInRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,7 +21,7 @@ import java.util.Optional;
 public class AttendeeService {
 
     private final AttendeeRepository attendeeRepository;
-    private final CheckInRepository checkInRepository;
+    private final CheckInService checkInService;
 
     public List<Attendee> getAllAttendeesFromEvent(String eventId){
         return this.attendeeRepository.findByEventId(eventId);
@@ -29,41 +32,41 @@ public class AttendeeService {
 
 //        Montando o AttendeeDetails de cada participante mas para tal temos que consultar checkins
         List<AttendeeDetails> attendeeDetailsList = attendeeList.stream().map(attendee -> {
-            Optional<CheckIn> checkIn = this.checkInRepository.findByAttendeeId(attendee.getId());
+            Optional<CheckIn> checkIn = this.checkInService.getCheckIn(attendee.getId());
             LocalDateTime checkedInAt = checkIn.<LocalDateTime>map(CheckIn::getCreatedAt).orElse(null);
             return new AttendeeDetails(attendee.getId(), attendee.getName(), attendee.getEmail(), attendee.getCreatedAt(), checkedInAt);
         }).toList();
 
         return new AttendeesListResponseDTO(attendeeDetailsList);
     }
-//
-//    public void verifyAttendeeSubscription(String email, String eventId){
-//        Optional<Attendee> isAttendeeRegistered = this.attendeeRepository.findByEventIdAndEmail(eventId, email);
-//        if(isAttendeeRegistered.isPresent()) throw new AttendeeAlreadyExistException("Attendee already registered");
-//
-//    }
-//
-//    public Attendee registerAttendee(Attendee newAttendee){
-//        this.attendeeRepository.save(newAttendee);
-//        return newAttendee;
-//    }
-//
-//    public void checkInAttendee(String attendeeId){
-//        Attendee attendee = this.getAttendee(attendeeId);
-//        this.checkInService.registerCheckIn(attendee);
-//    }
-//
-//    private Attendee getAttendee(String attendeeId){
-//        return this.attendeeRepository.findById(attendeeId).orElseThrow(() -> new AttendeeNotFoundException("Attendee not found with ID: " + attendeeId));
-//    }
-//
-//
-//    public AttendeeBadgeResponseDTO getAttendeeBadge(String attendeeId, UriComponentsBuilder uriComponentsBuilder){
-//        Attendee attendee = this.getAttendee(attendeeId);
-//
-//        var uri = uriComponentsBuilder.path("/attendees/{attendeeId}/check-in").buildAndExpand(attendeeId).toUri().toString();
-//
-//        AttendeeBadgeDTO attendeeBadgeDTO = new AttendeeBadgeDTO(attendee.getName(), attendee.getEmail(), uri, attendee.getEvent().getId());
-//        return new AttendeeBadgeResponseDTO(attendeeBadgeDTO);
-//    }
+
+    public void verifyAttendeeSubscription(String email, String eventId){
+        Optional<Attendee> isAttendeeRegistered = this.attendeeRepository.findByEventIdAndEmail(eventId, email);
+        if(isAttendeeRegistered.isPresent()) throw new AttendeeAlreadyExistException("O participante ja esta registrado!");
+
+    }
+
+    public Attendee registerAttendee(Attendee newAttendee){
+        this.attendeeRepository.save(newAttendee);
+        return newAttendee;
+    }
+
+    public void checkInAttendee(String attendeeId){
+        Attendee attendee = this.getAttendee(attendeeId);
+        this.checkInService.registerCheckIn(attendee);
+    }
+
+    private Attendee getAttendee(String attendeeId){
+        return this.attendeeRepository.findById(attendeeId).orElseThrow(() -> new AttendeeNotFoundException("Participante nao encotrado com o id: " + attendeeId));
+    }
+
+
+    public AttendeesListResponseDTO.AttendeeBadgeResponseDTO getAttendeeBadge(String attendeeId, UriComponentsBuilder uriComponentsBuilder){
+        Attendee attendee = this.getAttendee(attendeeId);
+
+        var uri = uriComponentsBuilder.path("/attendees/{attendeeId}/check-in").buildAndExpand(attendeeId).toUri().toString();
+
+        AttendeeBadgeDTO attendeeBadgeDTO = new AttendeeBadgeDTO(attendee.getName(), attendee.getEmail(), uri, attendee.getEvent().getId());
+        return new AttendeesListResponseDTO.AttendeeBadgeResponseDTO(attendeeBadgeDTO);
+    }
 }

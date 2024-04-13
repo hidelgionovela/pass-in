989eeb798f37aaf3e4c52786b66b7fdfe3567ede
hidelgionovela <hidelgionovela@gmail.com>
2024-04-13 21:05:28 +0200

    chore: update ExceptionEntityHandler

--- src/main/java/rocketseat/hdlg/passin/config/ExceptionEntityHandler.java
+++ src/main/java/rocketseat/hdlg/passin/config/ExceptionEntityHandler.java
@@ -4,7 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import rocketseat.hdlg.passin.domain.attendee.exceptions.AttendeeAlreadyExistException;
+import rocketseat.hdlg.passin.domain.attendee.exceptions.AttendeeNotFoundException;
+import rocketseat.hdlg.passin.domain.checkIn.excepctions.CheckInAlreadyExistsException;
+import rocketseat.hdlg.passin.domain.event.exceptions.EventFullException;
 import rocketseat.hdlg.passin.domain.event.exceptions.EventNotFoundException;
+import rocketseat.hdlg.passin.dto.general.ErrorResponseDTO;
 
 
 @ControllerAdvice //permite que você centralize a lógica de tratamento de exceções, personalização de vinculadores de dados e fornecimento de atributos globais em um único local
@@ -15,23 +20,23 @@ public class ExceptionEntityHandler {
         return ResponseEntity.notFound().build();
     }
 
-//    @ExceptionHandler(EventFullException.class)
-//    public ResponseEntity<ErrorResponseDTO> handleEventFull(EventFullException exception){
-//        return ResponseEntity.badRequest().body(new ErrorResponseDTO(exception.getMessage()));
-//    }
-//
-//    @ExceptionHandler(AttendeeNotFoundException.class)
-//    public ResponseEntity handleAttendeeFound(AttendeeNotFoundException exception){
-//        return ResponseEntity.notFound().build();
-//    }
-//
-//    @ExceptionHandler(AttendeeAlreadyExistException.class)
-//    public ResponseEntity handleAttendeeAlreadyExist(AttendeeAlreadyExistException exception){
-//        return ResponseEntity.status(HttpStatus.CONFLICT).build();
-//    }
-//
-//    @ExceptionHandler(CheckInAlreadyExistsException.class)
-//    public ResponseEntity handleCheckInAlreadyExist(CheckInAlreadyExistsException exception){
-//        return ResponseEntity.status(HttpStatus.CONFLICT).build();
-//    }
+    @ExceptionHandler(EventFullException.class)
+    public ResponseEntity<ErrorResponseDTO> handleEventFull(EventFullException exception){
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(exception.getMessage()));
+    }
+
+    @ExceptionHandler(AttendeeNotFoundException.class)
+    public ResponseEntity handleAttendeeFound(AttendeeNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AttendeeAlreadyExistException.class)
+    public ResponseEntity handleAttendeeAlreadyExist(AttendeeAlreadyExistException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @ExceptionHandler(CheckInAlreadyExistsException.class)
+    public ResponseEntity handleCheckInAlreadyExist(CheckInAlreadyExistsException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
 }

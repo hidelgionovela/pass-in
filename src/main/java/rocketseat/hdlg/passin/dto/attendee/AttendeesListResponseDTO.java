432b78aa8943d0c3a0cda690ab9a5be5ddfa3e7d
hidelgionovela <hidelgionovela@gmail.com>
2024-04-20 22:31:52 +0200

    Update AttendeesListResponseDTO.java

--- src/main/java/rocketseat/hdlg/passin/dto/attendee/AttendeesListResponseDTO.java
+++ src/main/java/rocketseat/hdlg/passin/dto/attendee/AttendeesListResponseDTO.java
@@ -2,6 +2,7 @@ package rocketseat.hdlg.passin.dto.attendee;
 
 import java.util.List;
 public record AttendeesListResponseDTO (List<AttendeeDetails> attendees) {
+
     public static record AttendeeBadgeResponseDTO(AttendeeBadgeDTO badge) {
 
     }
